3d95d66056efdff6162ac7e69195869b6bd45322
Shathish2004 <shathishkumaran07@gmail.com>
2026-06-30 00:21:12 +0530

    chore: remove webDir and reformat server configuration in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,10 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.shathish.personaltracker",
   appName: "Personal Tracker",
-  webDir: "out",
   server: {
     url: "https://personal-tracker-tau-puce.vercel.app",
-    cleartext: false
-  }
+    cleartext: false,
+  },
 };
 
 export default config;
